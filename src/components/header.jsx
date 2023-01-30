2a1574a4627ec8ef4704b5c6916c82efb680b6b1
Wiess <1053137796@qq.com>
2023-01-30 18:08:26 +0800

    22

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
           </Select>
         </div>
         <div className='right_header'>
-          <Image className='headportrait' src={'../assets/image/网易云音乐.png'}></Image>WonYve
+          <Image className='headportrait' src={'../assets/image/网易云音乐.png'}></Image>WonYves
         </div>
       </div>
     </Fragment>
